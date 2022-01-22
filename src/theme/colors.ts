79261e7b59866fdd82dd9bdde3a81c03a15922eb
MohammadArsalan <a4arshi@yahoo.com>
2022-01-22 12:51:18 +0500

    add vector icons and theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    primary: '#405DE6',
+
+    //common
+    black: '#000',
+    gray: '#666',
+    lightgray: '#ccc',
+    white: '#fff'
+}
---
+++ src/theme/fonts.ts
@@ -0,0 +1,17 @@
+export const size = {
+    xs: 10,
+    s:12,
+    default: 14,
+    md: 16,
+    lg: 20,
+    xlg: 24,
+    xxlg: 30
+}
+
+export const weight = {
+    full: '900',
+    semi: '600',
+    bold: 'bold',
+    normal: 'normal',
+    thin: '400',
+}
